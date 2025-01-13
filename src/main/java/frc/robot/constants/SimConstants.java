42be517541ac9e18db11083b20fd2e5795ba5eb9
Vismay Bhargav <vismay.bhargav@gmail.com>
2025-01-13 01:33:19 -0800

    (feat) add MapleSimSwerveDrivetrain injection + update vendordep

---
+++ src/main/java/frc/robot/constants/SimConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public class SimConstants {
+	public static final double MODULE_STEER_P = 70;
+	public static final double MODULE_STEER_D = 4.5;
+
+	public static final double DRIVE_FRICTION_VOLTS = 0.1;
+	public static final double STEER_FRICTION_VOLTS = 0.15;
+
+	public static final double STEER_INERTIA_KGMS2 = 0.05;
+}

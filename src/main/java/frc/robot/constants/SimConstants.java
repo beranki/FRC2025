f758a3c3dacd4a711f4a5b5a9971af50132e67bc
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-01-16 10:48:36 -0800

    (fix) change max speed to 6 and reset sim PID

--- src/main/java/frc/robot/constants/SimConstants.java
+++ src/main/java/frc/robot/constants/SimConstants.java
@@ -2,8 +2,8 @@ package frc.robot.constants;
 
 // These values are provided by Maple-Sim in order to reduce commonly found bugs
 public class SimConstants {
-	public static final double MODULE_STEER_P = 70;
-	public static final double MODULE_STEER_D = 4.5;
+	public static final double MODULE_STEER_P = 0;
+	public static final double MODULE_STEER_D = 0;
 
 	public static final double DRIVE_FRICTION_VOLTS = 0.1;
 	public static final double STEER_FRICTION_VOLTS = 0.15;
@@ -12,6 +12,6 @@ public class SimConstants {
 
 	public static final double SIM_LOOP_PERIOD = 0.05; // 5 ms
 
-	public static final double STARTING_POS_X_FT = 2.3;
-	public static final double STARTING_POS_Y_FT = 3.2;
+	public static final double STARTING_POS_X_FT = 5;
+	public static final double STARTING_POS_Y_FT = 5;
 }

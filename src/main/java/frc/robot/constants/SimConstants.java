075a7c6ef970e48e35c57dcb56df8c27cc174c11
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2025-01-17 14:30:11 -0800

    (nit) reset pids again

--- src/main/java/frc/robot/constants/SimConstants.java
+++ src/main/java/frc/robot/constants/SimConstants.java
@@ -2,8 +2,8 @@ package frc.robot.constants;
 
 // These values are provided by Maple-Sim in order to reduce commonly found bugs
 public class SimConstants {
-	public static final double MODULE_STEER_P = 0;
-	public static final double MODULE_STEER_D = 0;
+	public static final double MODULE_STEER_P = 70;
+	public static final double MODULE_STEER_D = 4.5;
 
 	public static final double DRIVE_FRICTION_VOLTS = 0.1;
 	public static final double STEER_FRICTION_VOLTS = 0.15;

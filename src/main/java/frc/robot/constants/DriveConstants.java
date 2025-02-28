786a7f4bb88902f762c4a2a40957bd09438e030a
Bhargav Eranki <bhargav.eranki@gmail.com>
2025-02-28 14:04:52 -0800

    prac-match-1

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -5,7 +5,7 @@ public class DriveConstants {
 	public static final double ROTATION_DEADBAND = 0.005;
 	public static final double JOYSTICK_DEADBAND = 0.1;
 
-	public static final double MAX_ANGULAR_VELO_RPS = 0.5;
+	public static final double MAX_ANGULAR_VELO_RPS = 0.75;
 
 	public static final double STEER_P = 100;
 	public static final double STEER_D = 0.5;
@@ -19,7 +19,7 @@ public class DriveConstants {
 	public static final double STEER_CURRENT_LIMIT = 40;
 
 	public static final double SPEED_DAMP_FACTOR = 6;
-	public static final double NORMAL_DAMP = TunerConstants.SPEED_AT_12_VOLTS.magnitude() / 4;
+	public static final double NORMAL_DAMP = TunerConstants.SPEED_AT_12_VOLTS.magnitude() / 4.5;
 
 	public static final double PASSIVE_ROBOT_FWD_M_S = 0.1;
 	public static final double SLEW_RATE = 2.9;

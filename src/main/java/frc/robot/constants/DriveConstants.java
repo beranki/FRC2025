4988914fa58dda7189d488916341a3569e920180
Aaron Dbritto <143990364+Adbritto@users.noreply.github.com>
2025-03-02 08:30:00 -0800

    Update DriveConstants.java
    Change current lims to 60 amps

--- src/main/java/frc/robot/constants/DriveConstants.java
+++ src/main/java/frc/robot/constants/DriveConstants.java
@@ -15,8 +15,8 @@ public class DriveConstants {
 	public static final double DRIVE_P = 0.1;
 	public static final double DRIVE_V = 0.124;
 
-	public static final double DRIVE_CURRENT_LIMIT = 120;
-	public static final double STEER_CURRENT_LIMIT = 40;
+	public static final double DRIVE_CURRENT_LIMIT = 60;
+	public static final double STEER_CURRENT_LIMIT = 60;
 
 	public static final double SPEED_DAMP_FACTOR = 6;
 	public static final double NORMAL_DAMP = TunerConstants.SPEED_AT_12_VOLTS.magnitude() / 4.5;
